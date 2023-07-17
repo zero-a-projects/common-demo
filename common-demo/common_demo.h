1238a48d8fe7f0d37185037078240decaeb0254b
xiaodong.wang <kk98.young@gmail.com>
2023-07-18 00:07:09 +0800

    Initial Commit

---
+++ common-demo/common_demo.h
@@ -0,0 +1,18 @@
+//
+//  common_demo.h
+//  common-demo
+//
+//  Created by Zero Wang on 2023/7/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for common_demo.
+FOUNDATION_EXPORT double common_demoVersionNumber;
+
+//! Project version string for common_demo.
+FOUNDATION_EXPORT const unsigned char common_demoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <common_demo/PublicHeader.h>
+
+
